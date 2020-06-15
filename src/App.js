d055bd9e5319e229b588950bbf7734f5ce457f48
Direnc <direnc99@gmail.com>
2020-06-15 19:13:17 +0200

    added particlesjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-
-function App() {
+import React from "react";
+import "./App.css";
+import Particles from "react-particles-js";
+const App = () => {
   return (
     <div className="App">
       Portfolio
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 50,
+            },
+            size: {
+              value: 3,
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode:"grab",
+              },
+            },
+          },
+        }}
+      />
     </div>
   );
-}
+};
 
 export default App;
